01049cd508cfb3e064b43376ffbc64814bb3af30
BagdauletKabylanbek <ytdiamond99@gmail.com>
2026-03-29 21:35:31 +0500

    lab9

--- lab8/api/urls.py
+++ lab8/api/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('products', views.ProductViewSet) 
+router.register('categories', views.CategoryViewSet)
+
 urlpatterns = [
-    path('products/', views.product_list),
-    path('product/<int:id>', views.product_detail),
-    path('categories/', views.category_list),
-    path('categories/<int:id>/', views.category_detail),
-    path('categories/<int:id>/products/', views.category_products),
+    path('', include(router.urls))
 ]
+
+
--- lab8/api/views.py
+++ lab8/api/views.py
@@ -1,33 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
-from django.http import JsonResponse
-from .models import Product, Category
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer 
 
-def product_list(request):
-    products = Product.objects.all()
-    data = list(products.values())
-    return JsonResponse(data, safe=False)
-
-def product_detail(request, id):
-    try:
-        product = Product.objects.get(pk=id)
-        return JsonResponse(list(Product.objects.filter(pk=id).values())[0])
-    except Product.DoesNotExist:
-        return JsonResponse({'error': 'Nor Found'}, status = 404)
-
-def category_list(request):
-    categoties = Category.objects.all()
-    data = list(categoties.values())
-    return JsonResponse(data, safe=False)
-
-def category_detail(request, id):
-    try:
-        return JsonResponse(list(Category.objects.filter(pk=id).values())[0])
-    except Category.DoesNotExist:
-        return JsonResponse({'error': 'Not Found'}, status = 404)
-
-def category_products(request, id):
-    products = Product.objects.filter(category_id = id)
-    data = list(products.values())
-    return JsonResponse(data, safe= False)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+    @action(detail=True, methods=['get'], url_path='products')
+    
+    def products(self, request, pk = None):
+        category = self.get_object()
+        qs = Product.objects.filter(category = category)
+        
+        serializer = ProductSerializer(qs, many = True)
+        return Response(serializer.data)
+    
